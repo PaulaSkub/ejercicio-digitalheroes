91a0d28f03c1523ae19e766e7da92cb6c4ec38c6
Paulaskub <poli.skubic@gmail.com>
2020-10-13 21:41:06 -0300

    punto 1 del ejercicio heroes digitales

--- myApp/routes/heroes.js
+++ myApp/routes/heroes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/heroes', function(req, res) {
-  res.render('heroes');
+  res.render('heroes',{});
 });
 
 module.exports = router;
